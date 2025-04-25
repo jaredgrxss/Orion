3004edbaf36e188164ccd26e9da6423f61f1c5c0
Jared <jaredgrossthe@gmail.com>
2025-04-24 21:46:50 -0500

    [JAR-13] integrating gem vulnerability analysis

--- lib/orion.rb
+++ lib/orion.rb
@@ -3,12 +3,9 @@
 require_relative "orion/version"
 require_relative "orion/cli/root"
 
-# Main module for Orion, starts up the CLI interface
-# With arguments passed upon invoking the exe (i.e orion <subcommand> command)
 module Orion
   class Error < StandardError; end
 
-  # CLI entry point - invoked from bin/orion
   def self.run(args = ARGV)
     Orion::CLI::Root.start(args)
   end
