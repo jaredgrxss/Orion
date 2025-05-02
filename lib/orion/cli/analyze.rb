2b0df8f115afab6bf16c5e63d7bb1197a305d089
Jared <jaredgrossthe@gmail.com>
2025-05-01 20:22:20 -0500

    [JAR-21] adding support for exporting JSON report of gems analysis

--- lib/orion/cli/analyze.rb
+++ lib/orion/cli/analyze.rb
@@ -3,6 +3,7 @@
 require "thor"
 require_relative "../gems/parser"
 require_relative "../helpers/renderer"
+require "pastel"
 
 module Orion
   module CLI
@@ -21,9 +22,16 @@ module Orion
 
         analyzed_gems, vulnerabilities = analyzer.run
 
+        pastel = Pastel.new
+
         case options[:format]
         when "json"
-          puts JSON.pretty_generate(result)
+          path = analyzer.export_gem_report(analyzed_gems, vulnerabilities, include_vulns: options[:include_vulns])
+          puts pastel.green("✔  Gems Analyzed: ") + pastel.white("#{analyzed_gems.size}")
+          if options[:include_vulns]
+            puts pastel.red("✖  Vulnerabilities Found: ") + pastel.white("#{vulnerabilities.size}")
+          end
+          puts pastel.cyan("📄 Report Saved To: ") + pastel.yellow(path)
         else
           puts Orion::Helpers::Renderer.render_table(rows: analyzed_gems)
           puts "\nAnalyzed #{analyzed_gems.size} gems, Found #{vulnerabilities.size} vulnerabilities"
